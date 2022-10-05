dd2584ec0d64db078987641adf1547f3b70e4bcc
Mig <angelorozo13@hotmail.com>
2022-10-04 22:22:28 -0500

    Navigation created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import icon from './assets/react.svg';
 
 const App = () => {
   const [connected, setConnected] = useState(false);
+  const [navVisible, setNavVisible] = useState(false);
+  const [currentConnection, setCurrentConnection] = useState('Japan');
 
   return (
     <main className={`Main ${connected ? 'active' : null}`}>
@@ -12,13 +14,102 @@ const App = () => {
         </svg>
       </button>
 
-      <button className="MainStatus" type="button">
+      <button className="MainStatus" type="button" onClick={ () => setNavVisible(!navVisible) }>
         <img className="MainStatus_icon" src={icon} alt="Status Icon" />
         <div className="MainStatus_label">
-          <strong>Japan</strong>
+          <strong>{currentConnection}</strong>
           <span>Connection {connected ? 'Enabled' : 'Disabled'}</span>
         </div>
       </button>
+
+      <aside className={`MainNav ${navVisible ? 'active' : null}`}>
+        <div className="MainNav_top">back-search</div>
+        <div className="MainNav_groups">
+          <h2 className="Subtitles">Automatic Connection (3)</h2>
+          <ul className="MainNav_list">
+            <li className="MainNav_item">
+              <input value="Japan" type="radio" name="current_item" id="item_id1" onChange={ () => {setCurrentConnection('Japan'); setNavVisible(false); setConnected(true); } } />
+              <label htmlFor="item_id1">
+                <img className="MainNav_item-icon" src={icon} alt="Item Icon" />
+                <strong className="MainNav_item-name">Japan</strong>
+                <span className="MainNav_item-speed">33%</span>
+              </label>
+            </li>
+            <li className="MainNav_item">
+              <input value="Canada" type="radio" name="current_item" id="item_id2" onChange={ () => {setCurrentConnection('Canada'); setNavVisible(false); setConnected(true); } } />
+              <label htmlFor="item_id2">
+                <img className="MainNav_item-icon" src={icon} alt="Item Icon" />
+                <strong className="MainNav_item-name">Canada</strong>
+                <span className="MainNav_item-speed">10%</span>
+              </label>
+            </li>
+            <li className="MainNav_item">
+              <input value="Australia" type="radio" name="current_item" id="item_id3" onChange={ () => {setCurrentConnection('Australia'); setNavVisible(false); setConnected(true); } } />
+              <label htmlFor="item_id3">
+                <img className="MainNav_item-icon" src={icon} alt="Item Icon" />
+                <strong className="MainNav_item-name">Australia</strong>
+                <span className="MainNav_item-speed">90%</span>
+              </label>
+            </li>
+          </ul>
+
+          <h2 className="Subtitles">Recent Connections (3)</h2>
+          <ul className="MainNav_list">
+            <li className="MainNav_item">
+              <input value="Japan" type="radio" name="current_item" id="item_id4" onChange={ () => {setCurrentConnection('Japan'); setNavVisible(false); setConnected(true); } } />
+              <label htmlFor="item_id4">
+                <img className="MainNav_item-icon" src={icon} alt="Item Icon" />
+                <strong className="MainNav_item-name">Japan</strong>
+                <span className="MainNav_item-speed">33%</span>
+              </label>
+            </li>
+            <li className="MainNav_item">
+              <input value="Canada" type="radio" name="current_item" id="item_id5" onChange={ () => {setCurrentConnection('Canada'); setNavVisible(false); setConnected(true); } } />
+              <label htmlFor="item_id5">
+                <img className="MainNav_item-icon" src={icon} alt="Item Icon" />
+                <strong className="MainNav_item-name">Canada</strong>
+                <span className="MainNav_item-speed">10%</span>
+              </label>
+            </li>
+            <li className="MainNav_item">
+              <input value="Australia" type="radio" name="current_item" id="item_id6" onChange={ () => {setCurrentConnection('Australia'); setNavVisible(false); setConnected(true); } } />
+              <label htmlFor="item_id6">
+                <img className="MainNav_item-icon" src={icon} alt="Item Icon" />
+                <strong className="MainNav_item-name">Australia</strong>
+                <span className="MainNav_item-speed">90%</span>
+              </label>
+            </li>
+          </ul>
+
+          <h2 className="Subtitles">All (10)</h2>
+          <ul className="MainNav_list">
+            <li className="MainNav_item">
+              <input value="Japan" type="radio" name="current_item" id="item_id7" onChange={ () => {setCurrentConnection('Japan'); setNavVisible(false); setConnected(true); } } />
+              <label htmlFor="item_id7">
+                <img className="MainNav_item-icon" src={icon} alt="Item Icon" />
+                <strong className="MainNav_item-name">Japan</strong>
+                <span className="MainNav_item-speed">33%</span>
+              </label>
+            </li>
+            <li className="MainNav_item">
+              <input value="Canada" type="radio" name="current_item" id="item_id8" onChange={ () => {setCurrentConnection('Canada'); setNavVisible(false); setConnected(true); } } />
+              <label htmlFor="item_id8">
+                <img className="MainNav_item-icon" src={icon} alt="Item Icon" />
+                <strong className="MainNav_item-name">Canada</strong>
+                <span className="MainNav_item-speed">10%</span>
+              </label>
+            </li>
+            <li className="MainNav_item">
+              <input value="Australia" type="radio" name="current_item" id="item_id9" onChange={ () => {setCurrentConnection('Australia'); setNavVisible(false); setConnected(true); } } />
+              <label htmlFor="item_id9">
+                <img className="MainNav_item-icon" src={icon} alt="Item Icon" />
+                <strong className="MainNav_item-name">Australia</strong>
+                <span className="MainNav_item-speed">90%</span>
+              </label>
+            </li>
+          </ul>
+        </div>
+      </aside>
     </main>
   );
 }
